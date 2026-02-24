6a8a5deadc28b6e75aa6201b6c5b278caf0e2264
Matheus <matheusbriporto@gmail.com>
2026-02-24 13:52:30 -0300

    Atualizando código do app

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { autoUpdater } = require("electron-updater")
 const { app, BrowserWindow, ipcMain } = require("electron")
 const path = require("path")
 
@@ -7,10 +8,13 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1100,
     height: 700,
+    minWidth: 900,
+    minHeight: 600,
     frame: false,
     transparent: true,
-    backgroundColor: "#00000000",
-    icon: path.join(__dirname, "assets/icon.ico"),
+    vibrancy: "under-window", // efeito glass no Mac
+    backgroundColor: "#00000000", // fundo transparente
+    titleBarStyle: "hidden",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
@@ -23,15 +27,30 @@ function createWindow() {
 
 app.whenReady().then(createWindow)
 
-ipcMain.on("minimize", () => mainWindow.minimize())
-ipcMain.on("maximize", () => {
-  if (mainWindow.isMaximized()) {
-    mainWindow.unmaximize()
-  } else {
-    mainWindow.maximize()
-  }
+app.whenReady().then(() => {
+
+  createWindow()
+
+  autoUpdater.checkForUpdatesAndNotify()
+
 })
-ipcMain.on("close", () => mainWindow.close())
+
+autoUpdater.on("update-available", () => {
+  console.log("Atualização disponível.")
+})
+
+autoUpdater.on("update-downloaded", () => {
+  console.log("Atualização baixada. Instalando...")
+  autoUpdater.quitAndInstall()
+})
+
+ipcMain.on("minimize-window", () => {
+  mainWindow.minimize()
+})
+
+ipcMain.on("close-window", () => {
+  mainWindow.close()
+})
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit()
